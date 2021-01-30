c51f517605eba48c68ca32a7a8cb97b8714571a5
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-01-30 13:28:50 -0500

    use script to create index

---
+++ make_table.py
@@ -0,0 +1,53 @@
+front = """
+<head>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <h1>Hello</h1>
+
+
+    <div id="users">
+        <input class="search" placeholder="Search" />
+        <button class="sort" data-sort="name">
+            Sort by name
+        </button>
+        <table>
+            <!-- IMPORTANT, class="list" have to be at tbody -->
+            <tbody class="list">
+"""
+
+end = """
+
+            </tbody>
+        </table>
+
+    </div>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
+
+    <script>
+        var options = {
+            valueNames: ['name', 'born']
+        };
+
+        var userList = new List('users', options);
+
+    </script>
+
+</body>
+"""
+
+html = front
+for i in range(100):
+    name = "x".ljust(i, "x")
+    row = f"""               
+                    <tr>
+                        <td class="name">{i} {name}</td>
+                        <td class="born">1983</td>
+                    </tr>
+    """
+    html = html + row
+
+html = html + end
+with open("index.html", "w") as file:
+    file.write(html)
